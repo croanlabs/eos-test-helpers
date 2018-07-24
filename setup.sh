22cefc01d979bd3b351843268446e146b5338e6e
etb <etb@etbs-MacBook-Pro.local>
2018-07-24 23:04:41 +0100

    Manually import EOS keys.

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # Start nodeos if it's not already running
 if [ -z "$(ps | grep nodeos)" ]
 then
-  echo | nohup nodeos -e -p eosio --plugin eosio::chain_api_plugin --plugin eosio::history_api_plugin > /root/nodeos.err 2> /root/nodeos.log &
+  echo | nohup nodeos -e -p eosio --plugin eosio::wallet_api_plugin --plugin eosio::wallet_plugin --plugin eosio::producer_plugin --plugin eosio::history_plugin --plugin eosio::chain_api_plugin --plugin eosio::history_api_plugin --plugin eosio::http_plugin -d /mnt/dev/data --config-dir /mnt/dev/config --http-server-address=0.0.0.0:8888 --access-control-allow-origin=* --contracts-console > /root/nodeos.err 2> /root/nodeos.log &
 fi
 
 # Create wallet and show password
@@ -13,7 +13,13 @@ WALLET_RES="$(cleos wallet create -n ${WALLET_NAME})"
 WALLET_PWD="$(grep -oP '(?<=\")(.*)(?=\")' <<< "$WALLET_RES")"
 echo Wallet password: ${WALLET_PWD}
 
+# Import eosio default key for the eos-dev docker image into your wallet so
+# that an account can be created
+echo "Importing eosio key into the new wallet so that accounts can be created..."
+echo 5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3 | cleos wallet import --name ${WALLET_NAME}
+
 # Create key and show public key
+echo "Creating keys for the new account..."
 WALLET_KEY_RES="$(cleos wallet create_key -n ${WALLET_NAME})"
 WALLET_PUBK="$(grep -oP '(?<=\")(.*)(?=\")' <<< "$WALLET_KEY_RES")"
 echo Wallet public key: ${WALLET_PUBK}
